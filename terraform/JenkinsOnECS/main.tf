ad1adea48e1603d8515a9282e808616b88e3b8bc
1021ky <2500024+1021ky@users.noreply.github.com>
2022-02-06 23:26:49 +0900

    fix: 正常にECSを実行できるようになったためコミット

--- terraform/JenkinsOnECS/main.tf
+++ terraform/JenkinsOnECS/main.tf
@@ -1,5 +1,3 @@
-
-}
 # 命名規則は右記を参考 https://dev.classmethod.jp/articles/aws-name-rule/
 
 provider "aws" {
@@ -17,12 +15,13 @@ terraform {
   required_version = ">= 0.14.9"
   backend "s3" {
     bucket = "dev.ksanchu"
-    key    = "terraform/backend/try_jenkins"
+    key    = "terraform/backend/try_jenkins/ECS"
     region = "us-west-1"
   }
 }
 
 # network
+
 
 resource "aws_vpc" "try-jenkins-on-ecs-dev-vpc" {
   cidr_block           = "10.0.0.0/16"
@@ -91,6 +90,7 @@ resource "aws_security_group" "try-jenkins-on-ecs-dev-sg" {
 }
 
 resource "aws_security_group_rule" "try-jenkins-on-ecs-dev-sg-rule01" {
+  # jenkinsへのアクセス許可
   type      = "ingress"
   from_port = 80
   to_port   = 8080
@@ -103,6 +103,7 @@ resource "aws_security_group_rule" "try-jenkins-on-ecs-dev-sg-rule01" {
 }
 
 resource "aws_security_group_rule" "try-jenkins-on-ecs-dev-sg-rule02" {
+  # ssh用許可
   type = "ingress"
 
   description       = "SSH from VPC"
@@ -136,21 +137,22 @@ resource "aws_ecs_task_definition" "try-jenkins-on-ecs-dev-ecs-task" {
   network_mode = "awsvpc"
 
   # ref https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/task_definition_parameters.html
-  container_definitions = <<EOL
-[
-  {
-    "name": "jenkins",
-    "image": "jenkins/jenkins:2.319.1-jdk11",
-    "portMappings": [
+  container_definitions = jsonencode(
+    [
       {
-        "containerPort": 80,
-        "hostPort": 8080
+        name  = "jenkins",
+        image = "jenkins/jenkins:2.319.1-jdk11",
+        portMappings = [
+          {
+            containerPort = 8080
+            hostPort      = 8080
+            protocol = "tcp"
+          }
+        ],
+        user = "jenkins"
       }
     ]
-    "user": "jenkins"
-  }
-]
-EOL
+  )
 }
 
 resource "aws_ecs_cluster" "try-jenkins-on-ecs-dev-ecs-cluster" {
@@ -161,80 +163,28 @@ resource "aws_ecs_service" "try-jenkins-on-ecs-dev-ecs-service" {
   name            = "try-jenkins-on-ecs-dev-ecs-service"
   cluster         = aws_ecs_cluster.try-jenkins-on-ecs-dev-ecs-cluster.name
   task_definition = aws_ecs_task_definition.try-jenkins-on-ecs-dev-ecs-task.arn
-  desired_count   = 3
+  desired_count   = 1
   # iam_role        = aws_iam_role.foo.arn
   # depends_on      = [aws_iam_role_policy.foo]
 
-  # ordered_placement_strategy {
-  #   type  = "binpack"
-  #   field = "cpu"
-  # }
-
-  # load_balancer {
-  #   target_group_arn = aws_lb_target_group.foo.arn
-  #   container_name   = "mongo"
-  #   container_port   = 8080
-  # }
-
-  # placement_constraints {
-  #   type       = "memberOf"
-  #   expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  # }
-}
-
-data "aws_ssm_parameter" "amazon-linux2-latest-ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
-
-resource "aws_instance" "try-jenkins-on-ecs-dev-ec2" {
-  ami           = data.aws_ssm_parameter.amazon-linux2-latest-ami.value
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.try-jenkins-on-ecs-dev-keypair.id
-  subnet_id     = aws_subnet.try-jenkins-on-ecs-dev-subnet01.id
-  vpc_security_group_ids = [
-    aws_security_group.try-jenkins-on-ecs-dev-sg.id
-  ]
-
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = self.public_ip
-      private_key = file("../jenkins_key_pair.pem")
-    }
-    inline = [
-      "sudo amazon-linux-extras install epel -y",
-      "sudo yum update –y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-      "sudo yum upgrade -y",
-      "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl start jenkins",
+  network_configuration {
+    # タスクの起動を許可するサブネット
+    subnets = [
+      aws_subnet.try-jenkins-on-ecs-dev-subnet01.id
+    ]
+    # タスクに紐付けるセキュリティグループ
+    security_groups = [
+      aws_security_group.try-jenkins-on-ecs-dev-sg.id
     ]
   }
-
-
-  tags = {
-    Name        = "try-jenkins-on-ecs-dev-ec2"
-    Env         = "dev"
-    ServiceName = "try-jenkins"
-  }
 }
 
-resource "aws_eip" "try-jenkins-on-ecs-dev-eip" {
-  instance = aws_instance.try-jenkins-on-ecs-dev-ec2.id
-  vpc      = true
 
-  tags = {
-    Name        = "try-jenkins-on-ecs-dev-ec2"
-    Env         = "dev"
-    ServiceName = "try-jenkins"
-  }
-}
+
 
 
 resource "aws_key_pair" "try-jenkins-on-ecs-dev-keypair" {
   key_name   = "try-jenkins-on-ecs-dev-keypair"
   public_key = file("./jenkins_key_pair.pub") # `ssh-keygen`コマンドで作成した公開鍵を指定
 }
+
